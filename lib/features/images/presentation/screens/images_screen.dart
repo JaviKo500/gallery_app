b71760047a3f6a0b64f31d06bfb16f4373fc4426
JaViKo500 <javikogutierrez64@gmail.com>
2025-03-22 22:03:04 -0500

    feat(ui): add card image view

--- lib/features/images/presentation/screens/images_screen.dart
+++ lib/features/images/presentation/screens/images_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
@@ -52,7 +54,7 @@ class ImagesScreen extends StatelessWidget {
                   shrinkWrap: true,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: Responsive.getValueBySize(context, ValueBySize(
-                    mobileValue: 3,
+                    mobileValue: 2,
                     tableValue: 5, 
                     desktopValue: 7
                     )
@@ -60,17 +62,10 @@ class ImagesScreen extends StatelessWidget {
                   mainAxisSpacing: 8.0, // spacing between rows
                   crossAxisSpacing: 8.0, // spacing between columns
                 ),
-                padding: EdgeInsets.all(8.0), // padding around the grid
+                // padding: EdgeInsets.all(4.0), // padding around the grid
                 itemCount: 20, // total number of items
                 itemBuilder: (context, index) {
-                  return Container(
-                    color: Colors.blue, // color of grid items
-                    child: Center(
-                      child: Text(
-                        'Item $index',
-                      ),
-                    ),
-                  );
+                  return _ImageCard();
                 },
                 )
               ],
@@ -78,6 +73,86 @@ class ImagesScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _ImageCard extends StatelessWidget {
+  const _ImageCard();
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    // color: Colors.grey[300],
+                  ),
+                  child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.network(
+                        'https://placehold.co/600x400.jpg',
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return Image.asset('assets/images/load.gif');
+                        },
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                    child: ClipRRect(  // <-- clips to the 200x200 [Container] below
+                      borderRadius: BorderRadius.circular(16),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 5.0,
+                          sigmaY: 5.0,
+                        ),
+                        child:  Container(
+                          alignment: Alignment.center,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Text(
+                            '6.2K files', 
+                            style: TypographyTheme.of(context).typography.bodySmall.copyWith(
+                              color: colorScheme.onPrimary,
+                              fontSize: 14,
+                            )
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 4,),
+        Text(
+          'Nature',
+          style: TypographyTheme.of(context).typography.bodyMedium,
+          overflow: TextOverflow.ellipsis,
+        )
+      ],
     );
   }
 }
