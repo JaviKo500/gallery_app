7d3ad3569c5d5179b38585a08987c4bd7186e78d
JaViKo500 <javikogutierrez64@gmail.com>
2025-03-22 21:02:14 -0500

    feat(ui): add filter options and grid view

--- lib/features/images/presentation/screens/images_screen.dart
+++ lib/features/images/presentation/screens/images_screen.dart
@@ -27,7 +27,7 @@ class ImagesScreen extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Padding(
           padding: customPaddingAllSize(context: context),
           child: Column(
@@ -44,14 +44,106 @@ class ImagesScreen extends StatelessWidget {
                   ),
                 )
               ),
-              Text(
-                'text',
-                style: TextStyle()
-              ),
+              _MenuFilter(),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: Responsive.getValueBySize(context, ValueBySize(
+                    mobileValue: 3,
+                    tableValue: 5, 
+                    desktopValue: 7
+                    )
+                  ), // number of items in each row
+                  mainAxisSpacing: 8.0, // spacing between rows
+                  crossAxisSpacing: 8.0, // spacing between columns
+                ),
+                padding: EdgeInsets.all(8.0), // padding around the grid
+                itemCount: 20, // total number of items
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: Colors.blue, // color of grid items
+                    child: Center(
+                      child: Text(
+                        'Item $index',
+                      ),
+                    ),
+                  );
+                },
+                ),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _MenuFilter extends StatelessWidget {
+  const _MenuFilter();
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Column(
+      children: [
+        SizedBox(height: 12,),
+        Row(
+          children: [
+            DropdownButton(
+              borderRadius: BorderRadius.circular(20),
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+              underline: Text(''),
+              icon: const Icon(Icons.keyboard_arrow_down_rounded),
+              value: 1,
+              items: [
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text('All'),
+                ),
+                DropdownMenuItem(
+                  value: 1,
+                  child: Text('Date modified'),
+                ),
+              ], 
+              onChanged: (value) {
+                
+              },
+            ),
+            Expanded(child: Container()),
+            Container(
+              height: 44,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                color: Colors.grey[300],
+              ),
+              child: 
+              Row(
+                children: [
+                  Container(
+                    width: 44,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: const Icon(Icons.image),
+                  ),
+                  Container(
+                    width: 44,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: colorScheme.primary,
+                    ),
+                    child: Icon(Icons.grid_view, color: colorScheme.onPrimary,),
+                  )
+                ]
+              ),
+            )
+          ],
+        ),
+        SizedBox(height: 4,),
+      ],
     );
   }
 }
